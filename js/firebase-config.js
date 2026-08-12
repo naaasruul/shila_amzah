ab16677dbad8ccbcf85839806f4e264c43df6d4b
naaasruul <nasrulhaqhidayat@gmail.com>
2026-08-12 16:38:03 +0800

    jangan la dey

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Replace these values with your Firebase project configuration.
+// You can get them from the Firebase console under Project settings.
+
+window.firebaseConfig = {
+    apiKey: "AIzaSyAgs2SeC0fj44SBaThbiZEAa1GJTUuzoj8",
+    authDomain: "shilaamzah-95e3a.firebaseapp.com",
+    projectId: "shilaamzah-95e3a",
+    storageBucket: "shilaamzah-95e3a.firebasestorage.app",
+    messagingSenderId: "6098169230",
+    appId: "1:6098169230:web:d98baf84060047c3d348df"
+  };
